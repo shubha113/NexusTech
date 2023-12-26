b1ca60d42baeb72163d01a73bcdbdeb68f676efd
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-12-26 19:12:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
-app.get("/",(req, res)=>res.send(`<h1>Site is working. click <a href=${process.env.FRONTEND_URL}></a> to visit frontend</h1>`))
+// Importing & Using Routes
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
 
 app.use(ErrorMiddleware);
