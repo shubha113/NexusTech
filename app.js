c1a249dd34c047d4d8f62a89ca8d9ce141db3ddf
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-12-28 18:08:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,13 @@ import cors from "cors";
 
 config({path:"./config/config.env"});
 const app = express();
-
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
 //using middlewares
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
 }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
-
 
 //importing and using routes
 import course from "./routes/courseRoutes.js";
