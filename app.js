acb2d3a7553564ce18164d87adbb5ee1ece88961
shubha113 <85664043+shubha113@users.noreply.github.com>
2023-12-28 18:12:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ import cors from "cors";
 
 config({path:"./config/config.env"});
 const app = express();
-app.use(cors({origin: process.env.FRONTEND_URL, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
+// Example middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 //using middlewares
 app.use(express.json());
 app.use(express.urlencoded({
