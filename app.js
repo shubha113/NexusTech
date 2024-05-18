bc2e1863a13b4cc01e039862a7df484893042b8f
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-18 13:46:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // Example middleware to enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
