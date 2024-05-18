e035e23493dd6f89c2c4ae7e8933bd24fd191429
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-18 12:06:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ import {config} from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 config({path:"./config/config.env"});
 const app = express();
-
-//cors code 
-app.use(cors({origin: process.env.FRONTEND_URL, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
-
+// Example middleware to enable CORS
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 //using middlewares
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
 }));
 app.use(cookieParser());
-
-
 //importing and using routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
@@ -27,8 +28,12 @@ app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
-
 export default app;
-app.get("/",(req, res)=>res.send(`<h1>Site is working. click <a href=${process.env.FRONTEND_URL}></a> to visit frontend</h1>`))
+// Importing & Using Routes
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
 
 app.use(ErrorMiddleware);
