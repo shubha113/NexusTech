b1721d56a7549130e9065ae06cbd404d05a059f6
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-18 13:03:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
 import express from "express";
-import { config } from "dotenv";
+import {config} from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-config({ path: "./config/config.env" });
-
+config({path:"./config/config.env"});
 const app = express();
-
 // Example middleware to enable CORS
-const allowedOrigins = [process.env.FRONTEND_URL, "https://nexustech-five.vercel.app"];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Check if the origin is in the allowed origins array
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-// Using middlewares
+//using middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true,
+}));
 app.use(cookieParser());
-
-// Importing and using routes
+//importing and using routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
 import payment from "./routes/paymentRoute.js";
 import other from "./routes/otherRoutes.js";
-
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
-
-// Home route
+export default app;
+// Importing & Using Routes
 app.get("/", (req, res) =>
   res.send(
-    `<h1>Site is Working. Click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
   )
 );
 
 app.use(ErrorMiddleware);
-
-export default app;
