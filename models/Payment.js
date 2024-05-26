b60871bfe8f2374527c648eba1b3ad5826a1b325
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-26 16:21:05 +0530

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -13,6 +13,11 @@ const schema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true,
+    },
     createdAt:{
         type: Date,
         default: Date.now,
