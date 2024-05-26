5837cf4bc2850f23e921573d73479e7c58c41f0d
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-27 01:14:21 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -50,6 +50,10 @@ const schema = new mongoose.Schema({
             poster:String,
         },
     ], 
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+      }],
     createdAt:{
         type:Date,
         default:Date.now,
