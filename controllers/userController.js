f260e55f943fc16fe582f47cb44735c35697968d
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-26 18:25:21 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,12 +56,12 @@ export const logout = catchAsyncError(async (req, res, next) => {
 });
 
 export const getMyProfile = catchAsyncError(async (req, res, next) => {
-    const user = await User.findById(req.user._id);
-    res.status(200).json({
-        success: true,
-        user
-      });
+  const user = await User.findById(req.user._id).populate('purchasedCourses');
+  res.status(200).json({
+      success: true,
+      user
   });
+});
 
 export const changePassword = catchAsyncError(async (req, res, next) => {
     const {oldPassword, newPassword} = req.body;
