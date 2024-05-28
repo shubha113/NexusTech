e70163cc61268fde93f2fc93f1d3b7f40953a0c1
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-28 10:43:26 +0530

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { addLecture, createCourse, deleteCourse, deleteLecture, getAllCourses, getCourseLectures } from '../controllers/courseController.js';
 import singleUpload from '../middlewares/multer.js';
 import { authorizeAdmin, authorizeSubscribers, isAuthenticated } from '../middlewares/auth.js';
-import { courseAccessMiddleware } from '../middlewares/courseAccess.js';
 const router = express.Router();
 
 
@@ -11,7 +10,7 @@ router.route('/courses').get(getAllCourses);
 //create new course -- admin
 router.route('/createcourse').post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
 //add lecture // delete course  //get course details
-router.route('/course/:id').get(isAuthenticated, authorizeSubscribers, courseAccessMiddleware, getCourseLectures)
+router.route('/course/:id').get(isAuthenticated, authorizeSubscribers, getCourseLectures)
 .post(isAuthenticated, authorizeAdmin, singleUpload, addLecture)
 .delete(isAuthenticated, authorizeAdmin, deleteCourse);
 //delete lecture
