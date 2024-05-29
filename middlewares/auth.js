6c01c7fed7d08813750a974737ae6e2279b486a7
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-05-30 01:13:58 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -32,40 +32,6 @@ export const authorizeAdmin = catchAsyncError(async (req, res, next) => {
     next();
   });
 
-  import jwt from 'jsonwebtoken';
-import { catchAsyncError } from './catchAsyncError.js';
-import ErrorHandler from '../utils/errorHandler.js';
-import { User } from '../models/User.js';
-
-export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
-
-  if (!token) {
-    return next(new ErrorHandler("Not Logged In", 401));
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded._id);
-
-    if (!req.user) {
-      // If user not found, consider it unauthorized
-      return next(new ErrorHandler("Not Logged In", 401));
-    }
-
-    next();
-  } catch (error) {
-    // Token verification failed
-    return next(new ErrorHandler("Not Logged In", 401));
-  }
-});
-
-export const authorizeAdmin = catchAsyncError(async (req, res, next) => {
-    if(req.user.role !== "admin")
-     return next(new ErrorHandler(`${req.user.role} is not allowed to access this resource`, 403));
-    next();
-  });
-
 export const authorizeSubscribers = catchAsyncError(async (req, res, next) => {
     if (req.user.role === "admin") {
         return next();
@@ -86,9 +52,3 @@ export const authorizeSubscribers = catchAsyncError(async (req, res, next) => {
 
     next();
 });
-
-    }
-  
-    next();
-  });
-  
