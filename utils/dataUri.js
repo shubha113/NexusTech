5e71b54e464a8c3f406d74c7cca79ef0e9555b1e
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-06-06 16:52:39 +0530

    Update dataUri.js

--- utils/dataUri.js
+++ utils/dataUri.js
@@ -25,6 +25,9 @@ export default getDataUri;
 export const generateCertificate = async (name, courseName, completionDate) => {
   const filePath = process.env.CERTIFICATE_TEMPLATE_PATH;
   if (!fs.existsSync(filePath)) {
+  return next(new ErrorHandler(`File not found: ${filePath}`, 500));
+}
+  if (!fs.existsSync(filePath)) {
     throw new Error(`File not found: ${filePath}`);
   }
   const existingPdfBytes = fs.readFileSync(filePath);
