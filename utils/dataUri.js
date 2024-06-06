11abb40aabcba082bda9b1043a51e66d7e3a52f6
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-06-07 00:52:06 +0530

    Update dataUri.js

--- utils/dataUri.js
+++ utils/dataUri.js
@@ -23,7 +23,7 @@ export default getDataUri;
 
 
 export const generateCertificate = async (name, courseName, completionDate) => {
-  const filePath = path.resolve(__dirname, process.env.CERTIFICATES_TEMPLATE_PATH);
+  const filePath = path.resolve(__dirname, '../../nexustech/src/assets/certificates/Untitled.pdf');
   console.log('Resolved template filePath:', filePath); // Debugging
 
   const existingPdfBytes = fs.readFileSync(filePath);
