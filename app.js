39b074c40b48643ab6eaa7a586dc09a44e1805b3
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-06-07 00:12:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,45 @@
-import express from "express";
-import {config} from "dotenv";
-import ErrorMiddleware from "./middlewares/Error.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import path from "path";
+import express from 'express';
+import { config } from 'dotenv';
+import ErrorMiddleware from './middlewares/Error.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
-config({path:"./config/config.env"});
+config({ path: './config/config.env' });
 const app = express();
 
-//cors code 
-app.use(cors({origin: process.env.FRONTEND_URL, credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
+// CORS configuration
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
-//using middlewares
+// Using middlewares
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true,
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 
 // Convert the module URL to a file path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use('/nexustech/src/assets/certiicates/', express.static(path.join(__dirname, process.env.CERTIFICATE_TEMPLATE_PATH)));
+const certificatesDir = path.resolve(__dirname, process.env.CERTIFICATES_DIR);
+app.use(process.env.CERTIFICATES_URL_BASE, express.static(certificatesDir));
 
-
-
-//importing and using routes
-import course from "./routes/courseRoutes.js";
-import tutorial from "./routes/tutorialRoutes.js";
-import question from "./routes/questionRoutes.js";
-import user from "./routes/userRoutes.js";
-import payment from "./routes/paymentRoute.js";
-import other from "./routes/otherRoutes.js";
-app.use("/api/v1", course);
-app.use("/api/v1", tutorial);
-app.use("/api/v1", question);
-app.use("/api/v1", user);
-app.use("/api/v1", payment);
-app.use("/api/v1", other);
-
+// Importing and using routes
+import course from './routes/courseRoutes.js';
+import tutorial from './routes/tutorialRoutes.js';
+import question from './routes/questionRoutes.js';
+import user from './routes/userRoutes.js';
+import payment from './routes/paymentRoute.js';
+import other from './routes/otherRoutes.js';
+app.use('/api/v1', course);
+app.use('/api/v1', tutorial);
+app.use('/api/v1', question);
+app.use('/api/v1', user);
+app.use('/api/v1', payment);
+app.use('/api/v1', other);
 
 export default app;
-app.get("/", (req, res) =>
+
+app.get('/', (req, res) =>
   res.send(
     `<h1>Site is Working. click <a href="${process.env.FRONTEND_URL}">here</a> to visit frontend.</h1>`
   )
