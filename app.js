591856d03510f42208fd6ea08e1aa6332610e12d
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-06-07 00:21:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ import { fileURLToPath } from 'url';
 config({ path: './config/config.env' });
 const app = express();
 
+// Print environment variables for debugging
+console.log('CERTIFICATES_DIR:', process.env.CERTIFICATES_DIR);
+console.log('CERTIFICATES_TEMPLATE_PATH:', process.env.CERTIFICATES_TEMPLATE_PATH);
+console.log('CERTIFICATES_URL_BASE:', process.env.CERTIFICATES_URL_BASE);
+
 // CORS configuration
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
@@ -21,6 +26,8 @@ app.use(cookieParser());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const certificatesDir = path.resolve(__dirname, process.env.CERTIFICATES_DIR);
+console.log('Resolved certificatesDir:', certificatesDir); // Debugging
+
 app.use(process.env.CERTIFICATES_URL_BASE, express.static(certificatesDir));
 
 // Importing and using routes
