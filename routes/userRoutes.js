81b4c50f0fa15cd0f034f7a22922f0c4578e2015
shubha113 <85664043+shubha113@users.noreply.github.com>
2024-06-06 16:11:40 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addToPlaylist, changePassword, deleteMyProfile, deleteUser, forgetPassword, getAllUsers, getMyProfile, login, logout, register, removeFromPlaylist, resetPassword, updateLectureProgress, updateProfile, updateProfilePicture, updateUserRole } from '../controllers/userController.js';
+import { addToPlaylist, changePassword, deleteMyProfile, deleteUser, forgetPassword, getAllUsers, generateCourseCertificate, getMyProfile, login, logout, register, removeFromPlaylist, resetPassword, updateLectureProgress, updateProfile, updateProfilePicture, updateUserRole } from '../controllers/userController.js';
 import {authorizeAdmin, isAuthenticated} from '../middlewares/auth.js';
 import singleUpload from '../middlewares/multer.js';
 
@@ -18,6 +18,8 @@ router.route('/me').get(isAuthenticated, getMyProfile);
 router.route('/me').delete(isAuthenticated, deleteMyProfile);
 //
 router.route('/me/updateProgress').post(isAuthenticated, updateLectureProgress);
+// Route to generate a certificate for a completed course
+router.route('/me/generate-certificate/:userId/:courseId').get ( isAuthenticated, generateCourseCertificate);
 //change password
 router.route('/changepassword').put(isAuthenticated, changePassword);
 //update profile
