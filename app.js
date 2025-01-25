b156d78563e318db09f14fc72fa7530b1aee376d
shubha113 <85664043+shubha113@users.noreply.github.com>
2025-01-25 13:10:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
